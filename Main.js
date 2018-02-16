550037696ff0481e8142550b14957233e45831fc
Daniel Avelares <danielavelares@gmail.com>
2018-02-16 14:25:40 -0700

    ESTABLISH CONNECTION TO MONGODB DATABASE
    created programData folder for database
    connected main.js file to mongo database

--- Main.js
+++ Main.js
@@ -1,11 +1,20 @@
 var http = require('express');
+var url = "mongodb://localhost:27017";
 
 const express = require('express')
+const mongoClient = require('mongodb').MongoClient;
 const app = express()
 app.use(express.static(__dirname + '/webFiles'));
 
 app.get('/', function (req, res) {
 	res.sendFile('/Index.html');
+});
+
+mongoClient.connect(url, function (err, db) {
+	if (!err) {
+		console.log('Connected to database');
+	}
+	db.close();
 });
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
